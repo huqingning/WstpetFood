3ed6aa1f60724e60343f0d5072d54ba65bf06b37
huqingning <820711660@qq.com>
2020-05-17 22:25:15 +0800

    第一次提交

---
+++ WstpetFoodServer/src/test/java/com/wst/wstpetdood/WstpetdoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wst.wstpetdood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WstpetdoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ wstweb/src/test/java/com/wst/wstweb/WstwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wst.wstweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WstwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
