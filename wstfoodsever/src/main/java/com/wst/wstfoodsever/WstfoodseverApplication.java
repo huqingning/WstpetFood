fb0c01adf84da8461b532fbb83d74bbb38f721be
huqingning <820711660@qq.com>
2020-05-18 15:49:54 +0800

    初始化

---
+++ wstfoodsever/src/main/java/com/wst/wstfoodsever/WstfoodseverApplication.java
@@ -0,0 +1,13 @@
+package com.wst.wstfoodsever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WstfoodseverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WstfoodseverApplication.class, args);
+    }
+
+}
---
+++ wstfoodsever/src/test/java/com/wst/wstfoodsever/WstfoodseverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wst.wstfoodsever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WstfoodseverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
