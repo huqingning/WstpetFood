dddaebb32823dfafd9cc5737d8a6967475860779
huqingning <820711660@qq.com>
2020-08-06 15:20:16 +0800

    ss
    ss

--- wstfoodsever/src/main/java/com/wst/wstfoodsever/WstfoodseverApplication.java
+++ wstfoodsever/src/main/java/com/wst/wstfoodsever/WstfoodseverApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-// mapper 接口类扫描包配置
+// mapper 接口类扫描包配置xxx
 @MapperScan("com.wst.wstfoodsever.mapper")
 public class WstfoodseverApplication {
 
